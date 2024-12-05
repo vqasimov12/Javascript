35e96ab7b7842d5acb623a329500b4bd3a9bc37c
vqasimov12 <q.vaqif1211@gmail.com>
2024-12-05 17:30:38 +0400

    javascript

--- lesson 1/index.js
+++ lesson 1/index.js
@@ -34,14 +34,45 @@
 //     console.log(n[0])
 // })
 
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-check(nums)
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+// check(nums)
 
-function check(a) {
-  for (let i = 0; i < a.length; i++)
-    
-    if (a[i] % 2) 
-        console.log(`${a[i]} is odd`)
-    else 
-        console.log(`${a[i]} is even`)
-}
+// function check(a) {
+//   for (let i = 0; i < a.length; i++)
+
+//     if (a[i] % 2)
+//         console.log(`${a[i]} is odd`)
+//     else
+//         console.log(`${a[i]} is even`)
+// }
+
+// kodun domdan sonra ishlemesi ucun ready yazilib
+$("#btn").click(() => {
+  const email1 = $("#email").val().trim();  // Corrected 'Trim' to 'trim'
+  const password1 = $("#password").val().trim();  // Corrected 'Trim' to 'trim'
+  signInRequest(email1, password1);
+});
+
+// Async function to send sign-in request
+const signInRequest = async (email1, password1) => {
+  try {
+    const response = await fetch("http://localhost:3000/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ email: email1, password: password1 }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Response not ok");
+    }
+
+    const data = await response.json();
+    window.location.href="./homePage.html"
+
+  } catch (error) {
+    console.error("Error occurred during request:", error);
+  }
+};
